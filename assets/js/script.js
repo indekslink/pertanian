a935d3bab4dbcac645e9d89d21e482b0beab1127
indekslink <aryainggil3@gmail.com>
2021-07-03 16:16:23 +0700

    Membuat rincian team

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,50 @@ window.onscroll = () => {
     .classList.toggle("show", window.scrollY > 50);
 };
 
-const menuDropdown = document.querySelector("li.link-dropdown");
 // menuDropdown.addEventListener("click", function () {
 //   this.classList.toggle("show");
 //   this.addEventListener("mouseleave", function () {
 //     this.classList.remove("show");
 //   });
 // });
-menuDropdown.addEventListener("click", function () {
-  this.classList.toggle("show");
-});
+
 window.onload = function () {
+  const menuDropdown = document.querySelectorAll("li.link-dropdown");
   if (window.innerWidth > 767) {
-    menuDropdown.children[0].addEventListener("mouseenter", function () {
-      this.parentElement.classList.toggle("show");
-    });
-    menuDropdown.children[1].addEventListener("mouseleave", function () {
-      this.parentElement.classList.toggle("show");
-    });
+    dropdownMouse(menuDropdown);
+  } else {
+    dropdownClick(menuDropdown);
   }
 };
+function dropdownMouse(dropdown) {
+  dropdown.forEach((mn) => {
+    mn.children[0].addEventListener("mouseenter", function () {
+      this.parentElement.classList.toggle("show");
+    });
+    mn.children[1].addEventListener("mouseleave", function () {
+      this.parentElement.classList.toggle("show");
+    });
+  });
+}
+function dropdownClick(dropdown) {
+  dropdown.addEventListener("click", function () {
+    this.classList.toggle("show");
+  });
+}
+
+$("#struktur-team .owl-carousel").owlCarousel({
+  stagePadding: 20,
+  margin: 5,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    569: {
+      items: 1,
+    },
+    767: {
+      items: 2,
+    },
+  },
+});
