41792b88f859f42562c3fd2e6a79ca0376a4b5f4
indekslink <aryainggil3@gmail.com>
2021-07-04 23:21:27 +0700

    Mengisi rincian tema

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,33 +19,16 @@ window.onscroll = () => {
 //   });
 // });
 
-window.onload = function () {
-  const menuDropdown = document.querySelectorAll("li.link-dropdown");
-  if (window.innerWidth > 767) {
-    dropdownMouse(menuDropdown);
-  } else {
-    dropdownClick(menuDropdown);
-  }
-};
-function dropdownMouse(dropdown) {
-  dropdown.forEach((mn) => {
-    mn.children[0].addEventListener("mouseenter", function () {
-      this.parentElement.classList.toggle("show");
-    });
-    mn.children[1].addEventListener("mouseleave", function () {
-      this.parentElement.classList.toggle("show");
-    });
+const menuDropdown = document.querySelectorAll("li.link-dropdown");
+menuDropdown.forEach((mn) => {
+  mn.children[0].addEventListener("click", function () {
+    this.parentElement.classList.toggle("show");
   });
-}
-function dropdownClick(dropdown) {
-  dropdown.addEventListener("click", function () {
-    this.classList.toggle("show");
-  });
-}
+});
 
 $("#struktur-team .owl-carousel").owlCarousel({
-  stagePadding: 20,
-  margin: 5,
+  stagePadding: 30,
+  margin: 10,
   dots: true,
   responsive: {
     0: {
@@ -54,8 +37,64 @@ $("#struktur-team .owl-carousel").owlCarousel({
     569: {
       items: 1,
     },
-    767: {
+    991: {
       items: 2,
+    },
+    1400: {
+      items: 3,
     },
   },
 });
+
+// link prevent defaul
+let linkMenu = Array.from(document.querySelectorAll("a.child-menu"));
+const linkLogo = document.querySelector(".my-nav .logo a");
+linkMenu.push(linkLogo);
+linkMenu.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+  });
+});
+
+// potong text yang memeiliki kelas show selengkapnya
+let dataText = document.querySelectorAll(".show-selengkapnya");
+dataText.forEach((text, i) => {
+  let valueTxt = text.innerHTML.split("");
+
+  let link = "/team/" + i;
+  let maxString = text.getAttribute("data-max-string");
+
+  let style =
+    text.getAttribute("data-style-selengkapnya") == "text"
+      ? textSelengkapnya(link)
+      : btnSelengkapnya(link);
+
+  if (valueTxt.length > parseInt(maxString)) {
+    valueTxt = valueTxt.slice(0, parseInt(maxString));
+    text.innerHTML = valueTxt.join("") + style;
+  }
+});
+function textSelengkapnya(link) {
+  return `<a href="${link}" class="text-small  text-decoration-none prevent-default">...selengkapnya</a>`;
+}
+function btnSelengkapnya(link) {
+  return ` <button
+            
+              class="hover-btn rounded text-small d-block prevent-default  mt-2 py-2 px-3"
+            >
+              <span>Selengkapnya</span>
+            </button>`;
+}
+
+let prevent = document.querySelectorAll(".prevent-default");
+
+prevent.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Maaf, Link masih belum berfungsi");
+  });
+});
+
+window.onload = function () {
+  document.querySelector(".loading").classList.add("close");
+};
